b7e8577face5ea0729accf4ae7b09e263540b2a3
juwon <rreone2017@gmail.com>
2022-03-26 08:38:22 +0900

    Initial commit

---
+++ app/src/main/java/com/brightGarden02/class1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.brightGarden02.class1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                // TODO: click event
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
