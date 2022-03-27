1a661d728fa7894eb22716ed4d13d090a392fa76
juwon <rreone2017@gmail.com>
2022-03-27 12:17:35 +0900

    회원가입 xml 구현( create: activity_join.xml update: colors.xml, styles.xml), 회원가입 데이터 값 요청 java 구현 (RegisterRequest.java )

---
+++ app/src/main/java/com/brightGarden02/class1/RegisterRequest.java
@@ -0,0 +1,31 @@
+// 회원가입 데이터 값 요청
+package com.brightGarden02.class1;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+
+    // 서버 URL 설정(php 파일 연동)
+    final static private String URL = "http://rreone2017.dothome.co.kr/Register.php";
+    private Map<String, String> map;
+
+    public RegisterRequest(String UserEmail, String UserPwd, String UserName, int UserAge, Response.Listener<String> listener){
+
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("UserEmail", UserEmail);
+        map.put("UserPwd", UserPwd);
+        map.put("UserName", UserName);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError{
+        return map;
+    }
+}
