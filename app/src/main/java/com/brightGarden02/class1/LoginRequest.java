8958a3e0dfec3013678234e7bc1dde866bf7cc05
juwon <rreone2017@gmail.com>
2022-03-28 12:42:09 +0900

    회원가입 처리(RegisterActivity.java) 아이디 중복 확인(ValidateRequest.java) 로그인 화면(activity_login.xml)
    로그인 데이터 값 요청(LoginRequest.java)
    로그인 처리(LoginActivity.java)

--- app/src/main/java/com/brightGarden02/class1/LoginRequest.java
+++ app/src/main/java/com/brightGarden02/class1/LoginRequest.java
@@ -1,4 +1,3 @@
-// 회원가입 데이터 값 요청
 package com.brightGarden02.class1;
 
 import com.android.volley.AuthFailureError;
@@ -8,20 +7,18 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterRequest extends StringRequest {
+public class LoginRequest extends StringRequest {
 
     // 서버 URL 설정(php 파일 연동)
-    final static private String URL = "http://rreone2017.dothome.co.kr/Register.php";
+    final static private String URL = "http://rreone2017.dothome.co.kr/Login.php";
     private Map<String, String> map;
 
-    public RegisterRequest(String UserEmail, String UserPwd, String UserName, Response.Listener<String> listener){
-
+    public LoginRequest(String UserEmail, String UserPwd, Response.Listener<String> listener){
         super(Method.POST, URL, listener, null);
 
         map = new HashMap<>();
         map.put("UserEmail", UserEmail);
         map.put("UserPwd", UserPwd);
-        map.put("UserName", UserName);
     }
 
     @Override
--- app/src/main/java/com/brightGarden02/class1/RegisterActivity.java
+++ app/src/main/java/com/brightGarden02/class1/RegisterActivity.java
@@ -1,15 +1,19 @@
 // 회원가입 처리
 package com.brightGarden02.class1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -33,6 +37,7 @@ public class RegisterActivity extends AppCompatActivity {
         join_name = findViewById(R.id.join_name);
         join_pwck = findViewById(R.id.join_pwck);
 
+
         // id 중복 체크
         check_button = findViewById(R.id.check_button);
         check_button.setOnClickListener(new View.OnClickListener(){
@@ -51,7 +56,7 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }
 
-                Response.Listener<String> respoonseListener = new Response.Listener<String>(){
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
 
                     @Override
                     public void onResponse(String response){
@@ -69,27 +74,98 @@ public class RegisterActivity extends AppCompatActivity {
 
                             }
                             else{
-
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("이미 존재하는 아이디입니다.").setPositiveButton("확인", null).create();
+                                dialog.show();
                             }
 
                         }
                         catch (JSONException e){
                             e.printStackTrace();
                         }
-
                     }
-
                 };
-
-
+                ValidateRequest validateRequest = new ValidateRequest(UserEmail, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(validateRequest);
             }
-
-
-
         });
 
 
+        // 회원가입 버튼 클릭시 수행
+        join_button = findViewById(R.id.join_button);
+        join_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                final String UserName = join_name.getText().toString();
+                final String UserEmail = join_email.getText().toString();
+                final String UserPwd = join_password.getText().toString();
+                final String PassCk = join_pwck.getText().toString();
+
+                // 아이디 중복체크 했는지 여부 확인
+                if (!validate) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("아이디 중복여부를 확인하세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+
+                }
+
+                // 이메일, 비밀번호, 비밀번호 입력 여부
+                if (UserEmail.equals("") || UserPwd.equals("") || UserName.equals("")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("모두 입력해주세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+
+                            // 비밀번호가 같은지 체크
+                            // 비밀번호가 같을시
+                            if (UserPwd.equals(PassCk)) {
+
+                                // 회원가입 성공시
+                                if (success) {
+                                    Toast.makeText(getApplicationContext(), String.format("%s님 가입을 환영합니다.", UserName), Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+                                }
+
+                                // 회원가입 실패시
+                                else {
+                                    Toast.makeText(getApplicationContext(), "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+
+                            // 비밀번호가 다를시
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("비밀번호가 동일하지 않습니다.").setNegativeButton("확인", null).create();
+                                dialog.show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                // 서버로 Volley를 이용해서 요청
+                RegisterRequest registerRequest = new RegisterRequest(UserEmail, UserPwd, UserName, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
-
-
 }
